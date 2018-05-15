fd3f84af4e066f8ee7a21dc196019988f628fc1a
aben20807 <aben20807@gmail.com>
2018-05-15 18:07:52 +0800

    Open a window and print out what mouse key being clicked

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (width, height) = (512, 512);
+    let mut window: PistonWindow =
+        WindowSettings::new("Title", [width, height]).exit_on_esc(true).build().unwrap();
+    while let Some(event) = window.next() {
+        if let Some(Button::Mouse(mouse_button)) = event.press_args() {
+            println!("Click!, {:?}", mouse_button);
+        }
+    }
 }
