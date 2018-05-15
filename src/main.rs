7ef611531bdc5d2ef77934f1d7c5c9b1865f0d9c
aben20807 <aben20807@gmail.com>
2018-05-15 19:02:33 +0800

    Draw a red rect in white background

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,16 @@ fn main() {
         if let Some(Button::Mouse(mouse_button)) = event.press_args() {
             println!("Click!, {:?}", mouse_button);
         }
+        window.draw_2d(&event, |c, g| {
+            clear([1.0, 1.0, 1.0, 0.0], g);
+            draw(&c, g);
+        });
     }
+}
+
+fn draw(c: &Context, g: &mut G2d) {
+    rectangle([1.0, 0.0, 0.0, 1.0],
+              [50.0, 0.0, 50.0, 100.0], // x, y, w, h
+              c.transform,
+              g);
 }
