15b6c3f0ff2fc73aedf33964570feb31a966de98
aben20807 <aben20807@gmail.com>
2018-05-17 23:35:21 +0800

    Add Game struct, and can call on_update and on_draw

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,50 @@ fn main() {
     let (width, height) = (512, 512);
     let mut window: PistonWindow =
         WindowSettings::new("Title", [width, height]).exit_on_esc(true).build().unwrap();
-    let mut rotation: f64 = 0.0;
+    // let mut rotation: f64 = 0.0;
+    let mut game = Game::new();
     while let Some(event) = window.next() {
-        if let Some(UpdateArgs { dt }) = event.update_args() {
-            rotation += 6.0 * dt;
+        if let Some(upd_args) = event.update_args() {
+            game.on_update(upd_args);
+            // rotation += 3.0 * upd_args.dt;
         }
+        // if let Some(render) = event.render_args() {
+        // game.on_draw(render, &event , &window);
+        // }
         if let Some(Button::Mouse(mouse_button)) = event.press_args() {
             println!("Click!, {:?}", mouse_button);
         }
-        window.draw_2d(&event, |c, g| {
-            clear([1.0, 1.0, 1.0, 0.0], g);
+        game.on_draw(&event , &mut window);
+        // window.draw_2d(&event, |c, g| {
+        //     clear([1.0, 1.0, 1.0, 0.0], g);
+        //     let center = c.transform.trans(256.0, 256.0);
+        //     let square = rectangle::square(0.0, 0.0, 100.0);
+        //     let red = [1.0, 0.0, 0.0, 1.0];
+        //     rectangle(red, square, center.rot_rad(rotation).trans(-50.0, -50.0), g);
+        // });
+    }
+}
+
+struct Game {
+    rotation: f64
+}
+
+impl Game {
+    fn new() -> Game {
+        Game { rotation : 0.0 }
+    }
+    fn on_update(&mut self, upd: UpdateArgs) {
+        self.rotation += 3.0 * upd.dt;
+    }
+    fn on_draw(&mut self,
+               event: &piston_window::Event,
+               window: &mut PistonWindow) {
+        window.draw_2d(event, |c, g| {
+            clear([0.0, 0.0, 0.0, 1.0], g);
             let center = c.transform.trans(256.0, 256.0);
             let square = rectangle::square(0.0, 0.0, 100.0);
             let red = [1.0, 0.0, 0.0, 1.0];
-            rectangle(red, square, center.rot_rad(rotation).trans(-50.0, -50.0), g);
+            rectangle(red, square, center.rot_rad(self.rotation).trans(-50.0, -50.0), g); // We translate the rectangle slightly so that it's centered; otherwise only the top left corner would be centered
         });
     }
 }
